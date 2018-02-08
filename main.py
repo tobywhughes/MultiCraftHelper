5229ec4588c7f73ac9be4a7144e1e48ccc4b6127
Toby Hughes <tobywhughes@gmail.com>
2018-02-08 00:18:51 -0800

    Improved documentation, formating, and warnings regarding security risks

--- main.py
+++ main.py
@@ -7,44 +7,55 @@ import os.path
 import sys
 
 def config_exists():
+    """Reads for all 3 extentions associated with shelve file. Returns True if the exist and False if at least one is not found"""
     extentions = ['bak', 'dat', 'dir']
+
     for extention in extentions:
         if not os.path.exists('settings/settings.config.' + extention):
             return False
     return True
 
 def get_config():
+    """Gets config settings from ./settings/settings.config and returns them"""
     if config_exists():
         return open_settings()
     else:
         raise FileNotFoundError
 
 def initialize_data():
+    """Initializes FTP and OS Filesystems and returns filesystem instances in tuple with ftp and os in that order"""
     try:
         settings = get_config()
     except FileNotFoundError as Error:
         print('ERROR: settings.config not found in settings folder. Please run settings.py')
         sys.exit(1)
+
+    # Checks for required user agreement. This user agreement details security risks associated with this program,
+    # so this program will not continue without confirmation that the user has read and agreed to said risks.
     if not settings[2]:
         print('ERROR: User has not read and agreed to risks with using this software. Please run settings.py')
         sys.exit(1)
+
+    #initializes filesystems
     ftp_settings, os_settings = settings[0], settings[1]
     ftp_fs = initialize_ftp(ftp_settings[0], ftp_settings[1], ftp_settings[2], ftp_settings[3])
     os_fs = initialize_os(os_settings)
+
     return (ftp_fs, os_fs)
 
 
-#Initialize FTP File System
 def initialize_ftp(username, password, host, port):
+    """Initializes FTP filesystem and returns associated instance"""
     ftp_fs = FTPFS(host, user=username, passwd=password, port=port)
     return ftp_fs
 
-#Initialize OS File System
 def initialize_os(directory):
+    """Initializes OS filesystem and returns associated instance"""
     os_fs = OSFS(directory)
     return os_fs
 
 def close_sessions(sessions):
+    """Closes sessions to all opened filesystems"""
     for session in sessions:
         session.close()
 
--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@ import colorama
 import os
 
 def security_agreement():
+    """Presents user with associated risks with FTP and prompts for explicet agreement to continue. Returns agreement as boolean"""
     agreement_text = (colorama.Fore.YELLOW + colorama.Back.RED +
                 'This program recieves backups from the MultiCraft servers over an FTP connection. FTP is UNSECURE and UNENCRPYTED. '
                 'It is vulnerable to man-in-the-middle attacks and password sniffing attacks.\nAgreeing to use this software means that '
@@ -12,6 +13,7 @@ def security_agreement():
                 'password associated with any other accounts you have.\nIf you still desire to use this software despite the associated '
                 'risks type YES (case-sensitive) to agree. Type NO to exit software. Otherwise, you will not be able to use this software.\n'
                 + colorama.Style.RESET_ALL + 'Please enter confirmation: ')
+
     while True:
         agreement_string = input(agreement_text)
         if agreement_string == 'YES':
@@ -21,14 +23,21 @@ def security_agreement():
         
 
 def prompt_user_ftp():
-    """Returns tuple containing username, password, host, and port in that order"""
+    """Prompts user for FTP information and returns tuple containing username, password, host, and port in that order"""
     username = input('Please Enter FTP Username:')
+
+    print(colorama.Fore.RED + 'FTP Passwords are sent over cleartext and this program stores your password in cleartext.\n'
+          'Do NOT use any password that you have associated with any other account or program.\n'
+          'If you do not agree to these storage or transmission conditions please exit this program now.' + colorama.Style.RESET_ALL)
     password = getpass.getpass('Please Enter FTP Password: ')
+
     host = input('Please Enter Host Address: ')
     port = int(input('Please Enter Port:'))
+
     return (username, password, host, port)
 
 def prompt_user_os():
+    """Prompts user for desired path for backups and returns path"""
     while True:
         path = input('Please Enter Desired Path. Enter nothing for default location (./backups): ')
         if path == '':
@@ -42,21 +51,31 @@ def prompt_user_os():
             
 
 def save_file(ftp_settings, os_settings, agreement):
+    """Saves settings in ./settings/settings.config shelve file"""
+
+    #Makes sure paths exist and creates them if not.
     if os_settings == os.path.abspath('./') + '/backups':
         os.makedirs('./backups', exist_ok=True)
     os.makedirs('./settings', exist_ok=True)
+
     shelf_file = shelve.open('settings/settings.config')
+
     shelf_file['ftp_settings'] = ftp_settings
     shelf_file['os_settings'] = os_settings
     shelf_file['agreement'] = agreement
+
     shelf_file.close()
 
 def open_settings():
+    """Opens ./settings/settings.config shelve file and returns settings as tupple with ftp_settings, os_settings, and agreement in that order"""
     shelf_file = shelve.open('settings/settings.config')
+
     ftp_settings = shelf_file['ftp_settings']
     os_settings = shelf_file['os_settings']
     agreement = shelf_file['agreement']
+
     shelf_file.close()
+    
     return (ftp_settings, os_settings, agreement)
 
 if __name__ == '__main__':
